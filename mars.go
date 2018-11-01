9375198ae5cb4491dc5efd8f5b926bf6a6330dd2
Mario Garcia <mario.ggar@gmail.com>
2018-11-01 19:33:45 +0100

    First commit

---
+++ mars.go
@@ -0,0 +1,13 @@
+// Package comment
+package main
+
+import "fmt"
+
+// function comment
+func main() {
+	fmt.Print("My weight on the surface of mars is ")
+	fmt.Print(149.0 * 0.3783)
+	fmt.Print(" lbs, and I would be ")
+	fmt.Print(41 * 365 / 687)
+	fmt.Print(" years old.")
+}
