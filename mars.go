86535899333dceee12b0b891abb744f8fa7a423f
Mario Garcia <mario.ggar@gmail.com>
2018-11-01 19:40:00 +0100

    Add var and const

--- mars.go
+++ mars.go
@@ -5,9 +5,16 @@ import "fmt"
 
 // function comment
 func main() {
-	fmt.Print("My weight on the surface of mars is ")
-	fmt.Print(149.0 * 0.3783)
-	fmt.Print(" lbs, and I would be ")
-	fmt.Print(41 * 365 / 687)
-	fmt.Print(" years old.")
+	var (
+		planet = "mars"
+		distance = 54.6
+	)
+
+	fmt.Printf("We're %v million kilometers away from %v.\n", distance, planet)
+
+	const weight = 149.0 * 0.3783
+	fmt.Printf("My weight on the surface of %v is %v lbs.\n", planet, weight)
+
+	const years = 41 * 365 / 687
+	fmt.Printf("And I would be %v years old.\n", years)
 }
